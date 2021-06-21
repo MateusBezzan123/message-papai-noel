04081ebaceafd540c7038cbe1cd7c889eb786700
Mateus Bezzan <mateusb@goatme.app>
2021-06-21 01:16:18 -0300

    Teste

--- node-sequileze/routes/index.js
+++ node-sequileze/routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/carta', function(req, res, next) {
+  res.render('index', { title: 'Carta' });
+});
+
+router.post('/carta/new', function(req, res, next) {
+  res.render('index', { title: 'Carta New' });
+});
+
+router.put('/carta/edit', function(req, res, next) {
+  res.render('index', { title: 'Carta Edit' });
+});
+
+router.delete('/carta', function(req, res, next) {
+  res.render('index', { title: 'Carta' });
+});
+
+
+
 module.exports = router;
